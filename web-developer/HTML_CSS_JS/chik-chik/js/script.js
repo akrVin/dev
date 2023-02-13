8394a784a21a9854554b7aad2dcbad726ed9a4f0
akrVin <akrvin@yandex.ru>
2023-02-13 15:09:08 +0500

    slider style

--- web-developer/HTML_CSS_JS/chik-chik/js/script.js
+++ web-developer/HTML_CSS_JS/chik-chik/js/script.js
@@ -1,32 +1,58 @@
 const addPreload = (elem) => {
-  elem.classList.add('preload')
+  elem.classList.add('preload') // added element preload
 };
 
 const removePreload = (elem) => {
-  elem.classList.remove('preload')
+  elem.classList.remove('preload') // remove element preload
 };
 
 const startSlider = (slider) => {
-  removePreload(slider);
+  const sliderItems = document.querySelectorAll('.slider__item'); // get all elements
+  // console.log('sliderItems', sliderItems);
+  const sliderList = document.querySelector('.slider__list'); // get class .slider__list
+  const btnPrevSlider = document.querySelector('.slider__arrow_left'); // get element button left
+  const btnNextSlider = document.querySelector('.slider__arrow_right'); // get element button right
+
+  let activeSlide = 1; // active slider
+  let position = 0; // the inital position of the slider 
+
+  const nextSlide = () => {
+    sliderItems[activeSlide]
+    // console.log('sliderItems[activeSlide]: ', sliderItems[activeSlide]);
+  };
+
+  const prevSlide = () => {
+    
+  };
+
+  btnPrevSlider.addEventListener('click', prevSlide);
+  btnNextSlider.addEventListener('click', nextSlide);
 }
 
 
 /* ================= */
 
 const initSlider = () => {
-  const slider = document.querySelector('.slider');
-
-  addPreload(slider)
+  const slider = document.querySelector('.slider'); // seatch class slider 
+  const sliderContainer = document.querySelector('.slider__container');
+  
+  sliderContainer.style.display = 'none';
+  addPreload(slider);
+  
   window.addEventListener('load', () => {
-    startSlider(slider);
+
+    sliderContainer.style.display = '';
+    removePreload(slider);
+
+    startSlider(slider); // 
   });
 };
 
 window.addEventListener('DOMContentLoaded', initSlider); // DOMContentLoaded - page loading widthout images
 initSlider(); // function call
 
-// window.addEventListener('load', initSlider);
+
 // console.log('window', window) // browsers window in console
 
 
-// console.log(slider) // getting an element
+// console.log(slider) // getting an elemenevenvt
