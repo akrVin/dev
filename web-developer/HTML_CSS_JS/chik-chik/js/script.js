1472990db177af9d2fffe4d9f1b9238b349543d1
akrVin <akrvin@yandex.ru>
2023-02-15 19:16:35 +0500

    block slider ready

--- web-developer/HTML_CSS_JS/chik-chik/js/script.js
+++ web-developer/HTML_CSS_JS/chik-chik/js/script.js
@@ -8,7 +8,6 @@ const removePreload = (elem) => {
 
 const startSlider = (slider) => {
   const sliderItems = document.querySelectorAll('.slider__item'); // get all elements
-  // console.log('sliderItems', sliderItems);
   const sliderList = document.querySelector('.slider__list'); // get class .slider__list
   const btnPrevSlider = document.querySelector('.slider__arrow_left'); // get element button left
   const btnNextSlider = document.querySelector('.slider__arrow_right'); // get element button right
@@ -17,32 +16,54 @@ const startSlider = (slider) => {
   let position = 0; // the inital position of the slider 
 
   const checkSlider =  () => {
-    if (activeSlide + 2 === sliderItems.length) {
-      btnNextSlider.remove();
+    if ((activeSlide + 2 === sliderItems.length && document.documentElement.offsetWidth > 560) || activeSlide === sliderItems.length) {
+      btnNextSlider.style.display = 'none';
+    } else {
+      btnNextSlider.style.display = '';
+    }
+
+    if (activeSlide === 1) {
+      btnPrevSlider.style.display = 'none';
+    } else {
+      btnPrevSlider.style.display = '';
     }
   } // scroll limit
 
+  checkSlider();
+
   const nextSlide = () => {
-    sliderItems[activeSlide].classList.remove('slider__item_active');
+    sliderItems[activeSlide]?.classList.remove('slider__item_active');
     position = -sliderItems[0].clientWidth * activeSlide;
 
     sliderList.style.transform = `translateX(${position}px)`;
     activeSlide += 1;
-    sliderItems[activeSlide].classList.add('slider__item_active');
+    sliderItems[activeSlide]?.classList.add('slider__item_active');
     checkSlider();
   };
 
   const prevSlide = () => {
-    sliderItems[activeSlide].classList.remove('slider__item_active');
+    sliderItems[activeSlide]?.classList.remove('slider__item_active');
     position = -sliderItems[0].clientWidth * (activeSlide - 2);
 
     sliderList.style.transform = `translateX(${position}px)`;
     activeSlide -= 1;
-    sliderItems[activeSlide].classList.add('slider__item_active');
+    sliderItems[activeSlide]?.classList.add('slider__item_active');
+    checkSlider();
   };
 
   btnPrevSlider.addEventListener('click', prevSlide);
   btnNextSlider.addEventListener('click', nextSlide);
+
+  window.addEventListener('resize', () => {
+    if (activeSlide + 2 > sliderItems.length && document.documentElement.offsetWidth > 560) {
+      activeSlide = sliderItems.length - 2;
+      sliderItems[activeSlide]?.classList.add('slider__item_active');
+    }
+
+    position = -sliderItems[0].clientWidth * (activeSlide - 1);
+    sliderList.style.transform = `translateX(${position}px)`;
+    checkSlider();
+  });
 }
 
 
@@ -68,7 +89,4 @@ window.addEventListener('DOMContentLoaded', initSlider); // DOMContentLoaded - p
 initSlider(); // function call
 
 
-// console.log('window', window) // browsers window in console
 
-
-// console.log(slider) // getting an elemenevenvt
