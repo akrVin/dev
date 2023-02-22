89841899d9cc1274706bffd3c297c6a7d178907e
akrVin <akrvin@yandex.ru>
2023-02-23 01:14:04 +0500

    added project YoreMeal

---
+++ web-developer/react/YourMeal_Vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { Container } from "./components/Container/Container";
+
+export const App = () => {
+
+  return (
+    <>
+      <header></header>
+      <main>
+        <nav>
+          <Container />
+        </nav>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
