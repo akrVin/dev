6bfe0a2a7ee3f1bc381cdc5fa53312982a54af0e
akrVin <37628803+akrVin@users.noreply.github.com>
2023-02-24 00:41:40 +0500

    update web-developer/react/YourMeal_Vite/src/components/Header/Header.jsx

--- web-developer/react/YourMeal_Vite/src/components/Header/Header.jsx
+++ web-developer/react/YourMeal_Vite/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { Container } from
 
 export const Header = () => (
     <header className={style.header}>
-    <Container className="header__container">
+    <Container className={header__container}>
       <img className="header__logo" src={logo}
       alt="Логотип YourMeal"/>
 
