4de52e8671aeefb637db7434d2f4bf7f42cd3774
akrVin <37628803+akrVin@users.noreply.github.com>
2023-02-24 00:36:56 +0500

    update web-developer/react/YourMeal_Vite/src/components/Header/Header.jsx

--- web-developer/react/YourMeal_Vite/src/components/Header/Header.jsx
+++ web-developer/react/YourMeal_Vite/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import styleHeader from "./Header.module.css";
 import logo from "./../../assets/img/logo.svg";
-import { Container } from "./../"
+import { Container } from "./../Container/"
 
 export const Header = () => (
     <header className="header">
