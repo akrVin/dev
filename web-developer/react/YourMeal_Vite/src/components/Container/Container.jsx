147f5e2b2e245d53be5a76da7be918e2af72328d
akrVin <37628803+akrVin@users.noreply.github.com>
2023-02-23 23:59:58 +0500

    update web-developer/react/YourMeal_Vite/src/components/Container/Container.jsx

--- web-developer/react/YourMeal_Vite/src/components/Container/Container.jsx
+++ web-developer/react/YourMeal_Vite/src/components/Container/Container.jsx
@@ -1,7 +1,7 @@
 import style from "./Container.modyle.css";
 import classNames from "classNames";
 
-export const Container = (className) => {
+export const Container = ({className}) => {
   return(
   <div className={style.container}></div>
   );
