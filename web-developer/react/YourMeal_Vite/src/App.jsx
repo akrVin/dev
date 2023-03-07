1ea4f570dcb6eaff03818442b7a22f4928475ce7
akrVin <akrvin@yandex.ru>
2023-03-07 12:54:07 +0500

    added footer

--- web-developer/react/YourMeal_Vite/src/App.jsx
+++ web-developer/react/YourMeal_Vite/src/App.jsx
@@ -1,6 +1,7 @@
 import { Catalog } from "./components/Catalog/Catalog";
 import { Header } from "./components/Header/Header";
 import { Navigation } from './components/Navigation/Navigation';
+import { Footer } from './components/Footer/Footer';
 
 
 
@@ -15,7 +16,7 @@ export const App = () => {
         <Navigation />
         <Catalog />
       </main>
-      <footer></footer>
+      <Footer />
     </>
   );
 }
