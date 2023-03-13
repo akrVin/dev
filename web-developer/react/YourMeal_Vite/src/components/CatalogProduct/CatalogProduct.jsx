77a75ed978c8a5d0801e34304ea8db139a9470b5
akrVin <37628803+akrVin@users.noreply.github.com>
2023-03-14 02:09:51 +0500

    update web-developer/react/YourMeal_Vite/src/components/CatalogProduct/CatalogProduct.jsx

--- web-developer/react/YourMeal_Vite/src/components/CatalogProduct/CatalogProduct.jsx
+++ web-developer/react/YourMeal_Vite/src/components/CatalogProduct/CatalogProduct.jsx
@@ -2,7 +2,7 @@ import style from './CatalogProduct.module.css';
 import { API_URI } from '../../const';
 
 export const CatalogProduct = ({ item }) => {
-  const dispatch = use
+  const dispatch = useDispatch(
 
   return (
 
